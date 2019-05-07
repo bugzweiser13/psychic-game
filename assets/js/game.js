bcb3d74012ceb2d72c4d0eae645acf94105983a4
bugzweiser <bugzweiser@gmail.com>
2019-05-06 23:44:22 -0700

    code correction

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,72 +5,56 @@ var usedArray = [];
 var ranLetter = ranLetter;
 var letters = "qwertyuiopasdfghjklzxcvbnm"
 
-//secret Letter generation 
+//letter generation 
 ranLetter = letters[Math.floor(Math.random() * letters.length)];
 console.log(ranLetter);
 
-//uses number generated with ranLetter to generate letter from var letters
+//uses number generated to pic from var letters
 function jsGuess() {
-      ranLetter = letters[Math.floor(Math.random() * letters.length)];
-      console.log(ranLetter);
+    ranLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(ranLetter);
 
 }
 
-//capturing keystrokes
-document.onkeyup = function (event) {
-      var playerGuess = event.key;
+//keystrokes
+document.onkeyup = function(event) {
+    var playerGuess = event.key;
 
-      //looks for match and tallies guesses.
-      if (playerGuess === ranLetter) {
-            won++;
-            attempts = 10;
-            usedArray = [];
+    //looks for match and tallies guesses.
+    if (playerGuess === ranLetter) {
+        won++;
+        attempts = 10;
+        usedArray = [];
 
-      }
-
-      //tallies incorrect guesses
-      jsGuess();
-      if (playerGuess !== ranLetter) {
-            attempts--;
-
-      }
-
-      //game reset if lost
-      if (attempts == 0) {
-            lost++;
-            usedArray = []
-            attempts = 10;
-      }
-
-
-      //prevents a letter to be printed a second time, but still tallies as a guess
-      if (usedArray.indexOf(playerGuess) >= 0) {
-
-      } else {
-            //writes incorrect guess
-            usedArray.push(playerGuess);
-            document.getElementById('playerGuess').innerHTML = usedArray;
-            console.log(usedArray);
-
-      }
-      //HTML output
-      document.getElementById('won').innerHTML = won;
-      document.getElementById('lost').innerHTML = lost;
-      document.getElementById('attempts').innerHTML = attempts;
-
-}
-function zeroPad(numberStr) {
-    return numberStr.padStart(2, "0");
-  }
-  
-  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  
-  numbers.forEach(
-    function(num) {
-      var numString = num.toString();
-      
-      var paddedNum = zeroPad(numString);
-  
-      console.log(paddedNum);
     }
-  );
+
+    //tallies incorrect guesses
+    jsGuess();
+    if (playerGuess !== ranLetter) {
+        attempts--;
+
+    }
+
+    //game reset if lost
+    if (attempts == 0) {
+        lost++;
+        usedArray = []
+        attempts = 10;
+    }
+
+
+    //prevents a letter to be printed a second time, but still tallies as a guess
+    if (usedArray.indexOf(playerGuess) >= 0) {
+
+    } else {
+        //writes incorrect guess
+        usedArray.push(playerGuess);
+        document.getElementById('playerGuess').innerHTML = usedArray;
+        console.log(usedArray);
+
+    }
+    //HTML output
+    document.getElementById('won').innerHTML = won;
+    document.getElementById('lost').innerHTML = lost;
+    document.getElementById('attempts').innerHTML = attempts;
+}
